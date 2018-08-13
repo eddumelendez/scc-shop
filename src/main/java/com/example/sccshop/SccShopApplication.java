f7c19393b01164d83d93b4fcc0afdea6de5b8c5e
Eddú Meléndez <eddu.melendez@gmail.com>
2018-08-12 23:10:59 -0500

    Initial commit

---
+++ src/main/java/com/example/sccshop/SccShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.sccshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccShopApplication.class, args);
+	}
+}
