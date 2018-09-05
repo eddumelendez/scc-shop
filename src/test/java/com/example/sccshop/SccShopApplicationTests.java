1f927b955c349bed4b329b85a504b925dca9a2db
Eddú Meléndez <eddu.melendez@gmail.com>
2018-09-05 18:59:51 -0500

    Move to juni5

--- src/test/java/com/example/sccshop/SccShopApplicationTests.java
+++ src/test/java/com/example/sccshop/SccShopApplicationTests.java
@@ -1,15 +1,15 @@
 package com.example.sccshop;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureStubRunner(ids = "com.example:scc-warehouse:+:8080", stubsMode = StubsMode.LOCAL)
